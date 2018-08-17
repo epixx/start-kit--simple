f644da3f4e0459a362d124b4c6b847871854a5a8
Nikolay Gromov <nicothin@gmail.com>
2018-08-17 19:18:03 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const mqpacker = require('css-mqpacker');
+const objectFitImages = require('postcss-object-fit-images');
+const browserSync = require('browser-sync').create();
+const ghPages = require('gulp-gh-pages');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const cleanCSS = require('gulp-cleancss');
+const wait = require('gulp-wait');
+
+let postCssPlugins = [
+  autoprefixer({
+    browsers: ['last 2 version'],
+    grid: true,
+  }),
+  mqpacker({
+    sort: true
+  }),
+  objectFitImages(),
+];
+
+gulp.task('style', function () {
+  return gulp.src('./scss/style.scss')
+    .pipe(plumber({
+      errorHandler: function(err) {
+        notify.onError({
+          title: 'Styles compilation error',
+          message: err.message
+        })(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(wait(100))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss(postCssPlugins))
+    .pipe(sourcemaps.write('/'))
+    .pipe(gulp.dest('./css/'))
+    .pipe(browserSync.stream({match: '**/*.css'}))
+    .pipe(rename('style.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default', ['serve']);
+
+gulp.task('serve', ['style'], function() {
+  browserSync.init({
+    server: './',
+    startPath: 'index.html',
+    port: 8080,
+    open: false
+  });
+
+  gulp.watch([
+    './scss/**/*.scss',
+  ], ['style']);
+
+  gulp.watch([
+    './*.html',
+  ], ['watch:html']);
+
+});
+
+gulp.task('watch:html', [], reload);
+
+gulp.task('deploy', function() {
+  return gulp.src([
+      './**/*',
+    ])
+    .pipe(ghPages());
+});
+
+function reload (done) {
+  browserSync.reload();
+  done();
+}
+
+var onError = function(err) {
+  notify.onError({
+    title: 'Error in ' + err.plugin,
+  })(err);
+  this.emit('end');
+};
---
+++ readme.md
@@ -0,0 +1,21 @@
+# EpicSkills start kit simple [![devDependencies Status](https://david-dm.org/epixx/start-kit--simple/dev-status.svg)](https://david-dm.org/epixx/start-kit--simple?type=dev) [![dependencies Status](https://david-dm.org/epixx/start-kit--simple/status.svg)](https://david-dm.org/epixx/start-kit--simple)
+
+Стартовый репозиторий.
+
+**ВНИМАНИЕ!** Не клонировать! Форкните себе этот репозиторий и клонируйте свой форк!
+
+Установка: `npm i`.
+
+Запуск: `npm start`.
+
+Остановка: <kbd>Ctrl + C</kbd>
+
+
+
+## К прочтению / ознакомлению
+
+- [Как работать с CSS-препроцессорами и БЭМ](http://nicothin.github.io/idiomatic-pre-CSS/)
+- [Шпаргалка по bash](https://github.com/nicothin/web-development/tree/master/bash)
+- [Шпаргалка по консольным командам Git](https://github.com/nicothin/web-development/tree/master/git)
+- [Шпаргалка ниндзя Sublime Text 3](http://nicothin.github.io/sublime-text/sublime-text-3-hotkeys.html)
+- [Sublime Text 3 для работы с фронтэндом](https://github.com/nicothin/sublime-text)
